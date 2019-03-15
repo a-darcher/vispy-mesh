4d2e0fdd95ddbd07227b030fd3142be89e3c1091
Alana Darcher <al@localhost.localdomain>
2019-03-15 18:03:41 +0100

    make-model as property

--- mesh.py
+++ mesh.py
@@ -4,19 +4,6 @@ import numpy as np
 
 np.set_printoptions(suppress=True, precision=2)
 
-def make_model_matrix(translate, rotation, scale): 
-	"""
-	returns 4x4 model matrix from translation, rotation, and data data 	
-	""" 
-	sm = tr.scale(scale).T 
-	rx,ry,rz = rotation 
-	rzm = tr.rotate(rz, [0,0,1]).T 
-	rym = tr.rotate(ry, [0,1,0]).T 
-	rxm = tr.rotate(rx, [1,0,0]).T 
-	trm = tr.translate(translate).T 
-	mm = trm @ rxm @ rym @ rzm @ sm  
-	return mm 
-
 class Mesh:
 	def __init__(self, obj_filename, position, rotation, scale):
 		vertices, faces, normals, texcoords = io.read_mesh('monkey.obj')
@@ -32,12 +19,20 @@ class Mesh:
 
 	@property	
 	def model_matrix(self):
-		return make_model_matrix(self.position, self.rotation, self.scale)
+		"""
+		returns 4x4 model matrix from translation, rotation, and data data 	
+		""" 
+		sm = tr.scale(scale).T 
+		rx,ry,rz = rotation 
+		rzm = tr.rotate(rz, [0,0,1]).T 
+		rym = tr.rotate(ry, [0,1,0]).T 
+		rxm = tr.rotate(rx, [1,0,0]).T 
+		trm = tr.translate(translate).T 
+		mm = trm @ rxm @ rym @ rzm @ sm  
+		return mm 
+	
 
 monkey = Mesh('monkey.obj', position=[1,2,3], rotation=[90,45,0], scale=[2,2,2])
 print(monkey.model_matrix)
 print(monkey.position)
-#mm = model_matrix = make_model_matrix([1,2,3], [90,45,0], [2,2,2])
-#print(mm)
-
 
